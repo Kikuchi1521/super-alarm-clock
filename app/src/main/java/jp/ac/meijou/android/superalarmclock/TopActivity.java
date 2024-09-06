e5cdddd572955945afc1429ccbb365bf8e77399a
Zen Kikuchi <231205075@ccmailg.meijo-u.ac.jp>
2024-09-06 14:17:33 +0900

    TopActivityから画面遷移できるように編集

--- app/src/main/java/jp/ac/meijou/android/superalarmclock/TopActivity.java
+++ app/src/main/java/jp/ac/meijou/android/superalarmclock/TopActivity.java
@@ -1,5 +1,6 @@
 package jp.ac.meijou.android.superalarmclock;
 
+import android.content.Intent;
 import android.os.Bundle;
 
 import androidx.activity.EdgeToEdge;
@@ -8,13 +9,28 @@ import androidx.core.graphics.Insets;
 import androidx.core.view.ViewCompat;
 import androidx.core.view.WindowInsetsCompat;
 
+import jp.ac.meijou.android.superalarmclock.databinding.ActivityTopBinding;
+
 public class TopActivity extends AppCompatActivity {
+
+    private ActivityTopBinding binding;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        EdgeToEdge.enable(this);
-        setContentView(R.layout.activity_top);
+        binding = ActivityTopBinding.inflate(getLayoutInflater());
+        setContentView(binding.getRoot());
+
+        binding.buttonSetInfo.setOnClickListener(view -> {
+            var intent = new Intent(this, SetInfoActivity.class);
+            startActivity(intent);
+        });
+
+        binding.buttonSchedule.setOnClickListener(view -> {
+            var intent = new Intent(this, ScheduleActivity.class);
+            startActivity(intent);
+        });
+
         ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main), (v, insets) -> {
             Insets systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars());
             v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom);
