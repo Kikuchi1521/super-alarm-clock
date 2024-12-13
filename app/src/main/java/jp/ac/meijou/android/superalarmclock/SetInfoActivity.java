dcd9759b2b7fa45f4406bb5d7c33097894c9c81f
Zen Kikuchi <231205075@ccmailg.meijo-u.ac.jp>
2024-12-13 16:14:03 +0900

    SetInfoActivity - 画面遷移しても保存したスピナーの値が保持されるように変更

--- app/src/main/java/jp/ac/meijou/android/superalarmclock/SetInfoActivity.java
+++ app/src/main/java/jp/ac/meijou/android/superalarmclock/SetInfoActivity.java
@@ -1,10 +1,16 @@
 package jp.ac.meijou.android.superalarmclock;
 
+import static android.content.ContentValues.TAG;
+
 import android.content.Intent;
 import android.os.Bundle;
+import android.util.Log;
 import android.view.LayoutInflater;
+import android.widget.Spinner;
+import android.widget.SpinnerAdapter;
 
 import androidx.activity.EdgeToEdge;
+import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
 import androidx.core.graphics.Insets;
 import androidx.core.view.ViewCompat;
@@ -22,8 +28,7 @@ public class SetInfoActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         binding = ActivitySetInfoBinding.inflate(getLayoutInflater());
         setContentView(binding.getRoot());
-        prefDataStore = PrefDataStore.getInstance(this
-        );
+        prefDataStore = PrefDataStore.getInstance(this);
 
         // Cancelボタンが押されたとき
         binding.buttonCancel.setOnClickListener(view -> {
@@ -38,6 +43,7 @@ public class SetInfoActivity extends AppCompatActivity {
             var thu = binding.spinnerThu.getSelectedItem().toString();
             var fri = binding.spinnerFri.getSelectedItem().toString();
             var sat = binding.spinnerSat.getSelectedItem().toString();
+            var commute = binding.editText.getSelectedItem().toString();
 
             prefDataStore.setString("mon", mon);
             prefDataStore.setString("tue", tue);
@@ -45,9 +51,9 @@ public class SetInfoActivity extends AppCompatActivity {
             prefDataStore.setString("thu", thu);
             prefDataStore.setString("fri", fri);
             prefDataStore.setString("sat", sat);
+            prefDataStore.setString("commute", commute);
 
-            var intent = new Intent(this, TopActivity.class);
-            startActivity(intent);
+            finish();
         });
 
         ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main), (v, insets) -> {
@@ -55,5 +61,37 @@ public class SetInfoActivity extends AppCompatActivity {
             v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom);
             return insets;
         });
+    }
+
+    @Override
+    protected void onStart() {
+        super.onStart();
+        prefDataStore.getString("mon")
+                        .ifPresent(mon -> setSelection(binding.spinnerMon, mon));
+        prefDataStore.getString("tue")
+                        .ifPresent(tue -> setSelection(binding.spinnerTue, tue));
+        prefDataStore.getString("wed")
+                        .ifPresent(wed -> setSelection(binding.spinnerWed, wed));
+        prefDataStore.getString("thu")
+                        .ifPresent(thu -> setSelection(binding.spinnerThu, thu));
+        prefDataStore.getString("fri")
+                        .ifPresent(fri -> setSelection(binding.spinnerFri, fri));
+        prefDataStore.getString("sat")
+                        .ifPresent(sat -> setSelection(binding.spinnerSat, sat));
+        prefDataStore.getString("commute")
+                        .ifPresent(time -> setSelection(binding.editText, time));
+        Log.d(TAG, "onResume: Change values of commute");
+    }
+
+    public static void setSelection(@NonNull Spinner spinner, String item) {
+        SpinnerAdapter adapter = spinner.getAdapter();
+        int idx = 0;
+        for (int i = 0; i < adapter.getCount(); i++) {
+            if (adapter.getItem(i).equals(item)) {
+                idx = i;
+                break;
+            }
+        }
+        spinner.setSelection(idx);
     }
 }
