b7178bb7fca3019ef7118d2892cd6b6e0ef86e18
Zen Kikuchi <231205075@ccmailg.meijo-u.ac.jp>
2024-12-13 23:41:41 +0900

    明日のアラーム時間が出るように設定

--- app/src/main/java/jp/ac/meijou/android/superalarmclock/TopActivity.java
+++ app/src/main/java/jp/ac/meijou/android/superalarmclock/TopActivity.java
@@ -31,71 +31,21 @@ public class TopActivity extends AppCompatActivity {
         setContentView(binding.getRoot());
         prefDataStore = PrefDataStore.getInstance(this);
 
+        Calendar tomorrow = Calendar.getInstance();
+        tomorrow.add(Calendar.DAY_OF_MONTH, 1);
+        display_tomorrow_alarm(tomorrow);
+
         binding.calendarView.setOnDateChangeListener(new CalendarView.OnDateChangeListener() {
             @Override
             public void onSelectedDayChange(@NonNull CalendarView view, int year, int month, int dayOfMonth) {
                 Calendar calendar = Calendar.getInstance();
                 calendar.set(year, month, dayOfMonth);
-                int day_of_week = calendar.get(Calendar.DAY_OF_WEEK);
 
                 int m = month + 1;
                 String date_display = m + "/" + dayOfMonth;
                 binding.textSelectedDay.setText(date_display);
 
-                AtomicReference<String> when_start = new AtomicReference<>("");
-                switch (day_of_week) {
-                    case (Calendar.SUNDAY):
-                        prefDataStore.getString("sun").ifPresent(sun -> when_start.set(sun));
-                        break;
-                    case (Calendar.MONDAY):
-                        prefDataStore.getString("mon").ifPresent(mon -> when_start.set(mon));
-                        break;
-                    case (Calendar.TUESDAY):
-                        prefDataStore.getString("tue").ifPresent(tue -> when_start.set(tue));
-                        break;
-                    case (Calendar.WEDNESDAY):
-                        prefDataStore.getString("wed").ifPresent(wed -> when_start.set(wed));
-                        break;
-                    case (Calendar.THURSDAY):
-                        prefDataStore.getString("thu").ifPresent(thu -> when_start.set(thu));
-                        break;
-                    case (Calendar.FRIDAY):
-                        prefDataStore.getString("fri").ifPresent(fri -> when_start.set(fri));
-                        break;
-                    case (Calendar.SATURDAY):
-                        prefDataStore.getString("sat").ifPresent(sat -> when_start.set(sat));
-                        break;
-                    default:
-                        when_start.set("error");
-                        break;
-                };
-
-                prefDataStore.getString("commute")
-                        .ifPresent(str_commute -> {
-
-                            LocalTime t;
-                            if (str_commute.equals("未設定")) {
-                                binding.textAlarmTime.setText("通学時間を設定してね");
-                            } else {
-
-                                int commute = Integer.parseInt(str_commute);
-                                t = switch (when_start.get()) {
-                                    case "1限" -> LocalTime.of(9, 0).plusMinutes(commute);
-                                    case "2限" -> LocalTime.of(10, 40).plusMinutes(commute);
-                                    case "3限" -> LocalTime.of(13, 0).plusMinutes(commute);
-                                    case "4限" -> LocalTime.of(14, 40).plusMinutes(commute);
-                                    case "5限" -> LocalTime.of(16, 20).plusMinutes(commute);
-                                    case "6限" -> LocalTime.of(18, 0).plusMinutes(commute);
-                                    default -> LocalTime.of(0, 0);
-                                };// switch
-
-                                if (t.equals(LocalTime.of(0,0))) {
-                                    binding.textAlarmTime.setText("アラームなし");
-                                } else {
-                                    timeConf(t, formatter);
-                                }
-                            }// if-else
-                        });
+                display_alarm(calendar);
             }// onSelectedDayChange
         });// calendarView.setOnDateChangeListener
 
@@ -125,5 +75,128 @@ public class TopActivity extends AppCompatActivity {
 
     private void timeConf(LocalTime t, DateTimeFormatter format) {
         binding.textAlarmTime.setText(t.format(format));
+    }
+
+    private void timeTomorrowConf(LocalTime t, DateTimeFormatter format) {
+        binding.textClock.setText(t.format(format));
+    }
+
+
+    private void display_alarm(Calendar calendar) {
+        int day_of_week = calendar.get(Calendar.DAY_OF_WEEK);
+
+        AtomicReference<String> when_start = new AtomicReference<>("");
+        switch (day_of_week) {
+            case (Calendar.SUNDAY):
+                prefDataStore.getString("sun").ifPresent(sun -> when_start.set(sun));
+                break;
+            case (Calendar.MONDAY):
+                prefDataStore.getString("mon").ifPresent(mon -> when_start.set(mon));
+                break;
+            case (Calendar.TUESDAY):
+                prefDataStore.getString("tue").ifPresent(tue -> when_start.set(tue));
+                break;
+            case (Calendar.WEDNESDAY):
+                prefDataStore.getString("wed").ifPresent(wed -> when_start.set(wed));
+                break;
+            case (Calendar.THURSDAY):
+                prefDataStore.getString("thu").ifPresent(thu -> when_start.set(thu));
+                break;
+            case (Calendar.FRIDAY):
+                prefDataStore.getString("fri").ifPresent(fri -> when_start.set(fri));
+                break;
+            case (Calendar.SATURDAY):
+                prefDataStore.getString("sat").ifPresent(sat -> when_start.set(sat));
+                break;
+            default:
+                when_start.set("error");
+                break;
+        };
+
+        prefDataStore.getString("commute")
+                .ifPresent(str_commute -> {
+
+                    LocalTime t;
+                    if (str_commute.equals("未設定")) {
+                        binding.textAlarmTime.setText("通学時間を設定してね");
+                    } else {
+
+                        int commute = Integer.parseInt(str_commute);
+                        t = switch (when_start.get()) {
+                            case "1限" -> LocalTime.of(9, 0).plusMinutes(commute);
+                            case "2限" -> LocalTime.of(10, 40).plusMinutes(commute);
+                            case "3限" -> LocalTime.of(13, 0).plusMinutes(commute);
+                            case "4限" -> LocalTime.of(14, 40).plusMinutes(commute);
+                            case "5限" -> LocalTime.of(16, 20).plusMinutes(commute);
+                            case "6限" -> LocalTime.of(18, 0).plusMinutes(commute);
+                            default -> LocalTime.of(0, 0);
+                        };// switch
+
+                        if (t.equals(LocalTime.of(0,0))) {
+                            binding.textAlarmTime.setText("アラームなし");
+                        } else {
+                            timeConf(t, formatter);
+                        }
+                    }// if-else
+                });
+    }
+
+    private void display_tomorrow_alarm(Calendar calendar) {
+        int day_of_week = calendar.get(Calendar.DAY_OF_WEEK);
+
+        AtomicReference<String> when_start = new AtomicReference<>("");
+        switch (day_of_week) {
+            case (Calendar.SUNDAY):
+                prefDataStore.getString("sun").ifPresent(sun -> when_start.set(sun));
+                break;
+            case (Calendar.MONDAY):
+                prefDataStore.getString("mon").ifPresent(mon -> when_start.set(mon));
+                break;
+            case (Calendar.TUESDAY):
+                prefDataStore.getString("tue").ifPresent(tue -> when_start.set(tue));
+                break;
+            case (Calendar.WEDNESDAY):
+                prefDataStore.getString("wed").ifPresent(wed -> when_start.set(wed));
+                break;
+            case (Calendar.THURSDAY):
+                prefDataStore.getString("thu").ifPresent(thu -> when_start.set(thu));
+                break;
+            case (Calendar.FRIDAY):
+                prefDataStore.getString("fri").ifPresent(fri -> when_start.set(fri));
+                break;
+            case (Calendar.SATURDAY):
+                prefDataStore.getString("sat").ifPresent(sat -> when_start.set(sat));
+                break;
+            default:
+                when_start.set("error");
+                break;
+        };
+
+        prefDataStore.getString("commute")
+                .ifPresent(str_commute -> {
+
+                    LocalTime t;
+                    if (str_commute.equals("未設定")) {
+                        binding.textClock.setText("通学時間を設定してね");
+                    } else {
+
+                        int commute = Integer.parseInt(str_commute);
+                        t = switch (when_start.get()) {
+                            case "1限" -> LocalTime.of(9, 0).plusMinutes(commute);
+                            case "2限" -> LocalTime.of(10, 40).plusMinutes(commute);
+                            case "3限" -> LocalTime.of(13, 0).plusMinutes(commute);
+                            case "4限" -> LocalTime.of(14, 40).plusMinutes(commute);
+                            case "5限" -> LocalTime.of(16, 20).plusMinutes(commute);
+                            case "6限" -> LocalTime.of(18, 0).plusMinutes(commute);
+                            default -> LocalTime.of(0, 0);
+                        };// switch
+
+                        if (t.equals(LocalTime.of(0,0))) {
+                            binding.textClock.setText("アラームなし");
+                        } else {
+                            timeTomorrowConf(t, formatter);
+                        }
+                    }// if-else
+                });
     }
 }
