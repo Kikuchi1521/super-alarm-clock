84dc37dd80a6c04ce14f63dbbdc8897268d4f512
Zen Kikuchi <231205075@ccmailg.meijo-u.ac.jp>
2024-12-13 23:20:06 +0900

    設定時刻に合わせて自動でアラーム時刻を計算して表示するように更新

--- app/src/main/java/jp/ac/meijou/android/superalarmclock/TopActivity.java
+++ app/src/main/java/jp/ac/meijou/android/superalarmclock/TopActivity.java
@@ -2,31 +2,102 @@ package jp.ac.meijou.android.superalarmclock;
 
 import android.content.Intent;
 import android.os.Bundle;
+import android.widget.CalendarView;
 
-import androidx.activity.EdgeToEdge;
-import androidx.activity.result.ActivityResultLauncher;
-import androidx.activity.result.contract.ActivityResultContracts;
+import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
 import androidx.core.graphics.Insets;
 import androidx.core.view.ViewCompat;
 import androidx.core.view.WindowInsetsCompat;
 
+import java.time.LocalTime;
+import java.time.format.DateTimeFormatter;
 import java.util.Calendar;
-import java.util.Optional;
+import java.util.concurrent.atomic.AtomicReference;
 
 import jp.ac.meijou.android.superalarmclock.databinding.ActivityTopBinding;
 
 public class TopActivity extends AppCompatActivity {
 
     private ActivityTopBinding binding;
+    private PrefDataStore prefDataStore;
+
+    DateTimeFormatter formatter = DateTimeFormatter.ofPattern("HH:mm");
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         binding = ActivityTopBinding.inflate(getLayoutInflater());
         setContentView(binding.getRoot());
+        prefDataStore = PrefDataStore.getInstance(this);
 
-        timeConf("00", "00");
+        binding.calendarView.setOnDateChangeListener(new CalendarView.OnDateChangeListener() {
+            @Override
+            public void onSelectedDayChange(@NonNull CalendarView view, int year, int month, int dayOfMonth) {
+                Calendar calendar = Calendar.getInstance();
+                calendar.set(year, month, dayOfMonth);
+                int day_of_week = calendar.get(Calendar.DAY_OF_WEEK);
+
+                int m = month + 1;
+                String date_display = m + "/" + dayOfMonth;
+                binding.textSelectedDay.setText(date_display);
+
+                AtomicReference<String> when_start = new AtomicReference<>("");
+                switch (day_of_week) {
+                    case (Calendar.SUNDAY):
+                        prefDataStore.getString("sun").ifPresent(sun -> when_start.set(sun));
+                        break;
+                    case (Calendar.MONDAY):
+                        prefDataStore.getString("mon").ifPresent(mon -> when_start.set(mon));
+                        break;
+                    case (Calendar.TUESDAY):
+                        prefDataStore.getString("tue").ifPresent(tue -> when_start.set(tue));
+                        break;
+                    case (Calendar.WEDNESDAY):
+                        prefDataStore.getString("wed").ifPresent(wed -> when_start.set(wed));
+                        break;
+                    case (Calendar.THURSDAY):
+                        prefDataStore.getString("thu").ifPresent(thu -> when_start.set(thu));
+                        break;
+                    case (Calendar.FRIDAY):
+                        prefDataStore.getString("fri").ifPresent(fri -> when_start.set(fri));
+                        break;
+                    case (Calendar.SATURDAY):
+                        prefDataStore.getString("sat").ifPresent(sat -> when_start.set(sat));
+                        break;
+                    default:
+                        when_start.set("error");
+                        break;
+                };
+
+                prefDataStore.getString("commute")
+                        .ifPresent(str_commute -> {
+
+                            LocalTime t;
+                            if (str_commute.equals("未設定")) {
+                                binding.textAlarmTime.setText("通学時間を設定してね");
+                            } else {
+
+                                int commute = Integer.parseInt(str_commute);
+                                t = switch (when_start.get()) {
+                                    case "1限" -> LocalTime.of(9, 0).plusMinutes(commute);
+                                    case "2限" -> LocalTime.of(10, 40).plusMinutes(commute);
+                                    case "3限" -> LocalTime.of(13, 0).plusMinutes(commute);
+                                    case "4限" -> LocalTime.of(14, 40).plusMinutes(commute);
+                                    case "5限" -> LocalTime.of(16, 20).plusMinutes(commute);
+                                    case "6限" -> LocalTime.of(18, 0).plusMinutes(commute);
+                                    default -> LocalTime.of(0, 0);
+                                };// switch
+
+                                if (t.equals(LocalTime.of(0,0))) {
+                                    binding.textAlarmTime.setText("アラームなし");
+                                } else {
+                                    timeConf(t, formatter);
+                                }
+                            }// if-else
+                        });
+            }// onSelectedDayChange
+        });// calendarView.setOnDateChangeListener
 
         binding.buttonSetInfo.setOnClickListener(view -> {
             var intent = new Intent(this, SetInfoActivity.class);
@@ -45,7 +116,14 @@ public class TopActivity extends AppCompatActivity {
         });
     }
 
-    private void timeConf(String h, String m) {
-        binding.textClock.setText(h + ":" + m);
+    @Override
+    protected void onResume() {
+        super.onResume();
+        binding.textSelectedDay.setText("");
+        binding.textAlarmTime.setText("日付を選択...");
+    }
+
+    private void timeConf(LocalTime t, DateTimeFormatter format) {
+        binding.textAlarmTime.setText(t.format(format));
     }
 }
